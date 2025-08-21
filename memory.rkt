f9ebb5a007e61faaf7e833227b6aff1fde0ef45e
yasna-nsh <y.noushirvani@gmail.com>
2025-08-21 13:42:30 +0800

    added create to memory

--- memory.rkt
+++ memory.rkt
@@ -1,54 +1,63 @@
 #lang racket
 (require (lib "eopl.ss" "eopl"))
 
-(define N 100)
+; (define N 100)
 
 (define (report-out-of-memory!) (eopl:error 'newref "\n\tmemory is full!"))
 (define (report-unauthorized-access-to-memory! var) (eopl:error 'unauthorized-access-to-memory "\n\tlocation ~s hasn't been allocated!" var))
-(define (report-memory-index-out-of-range! var) (eopl:error 'memory-index-out-of-range "\n\tindex ~s isn't in range 0 to ~s!" var (- N 1)))
+(define (report-memory-index-out-of-range! idx N) (eopl:error 'memory-index-out-of-range "\n\tindex ~s isn't in range 0 to ~s!" idx (- N 1)))
 
+(struct memory (size store free-addr-pool) #:mutable)
+; (define-datatype memory memory?
+;   (a-memory (size number?) (store vector?) (free-addr-pool list?)))
+  
+; (define store (make-vector N))
+; (define free-addr-pool (range 0 (- N 1)))
 
-(define memory (make-vector N))
-(define free-addr-pool (range 0 (- N 1)))
-
+; returns a memory struct, hopefully mutable
+(define create-memory
+  (lambda (N)
+    (let ([store (make-vector N)] [free-addr-pool (range 0 N)])
+      (memory N store free-addr-pool))))
 
 (define in-range?
-  (lambda (index)
-    (and (>= index 0) (< index N))))
+  (lambda (mem index)
+    (and (>= index 0) (< index (memory-size mem)))))
 
 (define newref
-  (lambda ()
-    (if (null? free-addr-pool) 
-        (report-out-of-memory!)
-        (let ([alloc-addr (first free-addr-pool)])
-          (set! free-addr-pool (rest free-addr-pool))
-          alloc-addr))))
+  (lambda (mem)
+    (let ([free-addr-pool (memory-free-addr-pool mem)])
+      (if (null? free-addr-pool) 
+          (report-out-of-memory!)
+          (let ([alloc-addr (first free-addr-pool)])
+            (set-memory-free-addr-pool! mem (rest free-addr-pool))
+            alloc-addr)))))
 
 (define newref-init
-  (lambda (init-val)
-    (let ([index (newref)])
-      (vector-set! memory index init-val)
+  (lambda (mem init-val)
+    (let ([index (newref mem)])
+      (vector-set! (memory-store mem) index init-val)
       index)))
 
 (define assign
-  (lambda (index val)
+  (lambda (mem index val)
     (cond
-      [(not (in-range? index)) (report-memory-index-out-of-range! index)]
-      [(member index free-addr-pool) (report-unauthorized-access-to-memory! index)]
-      [else (vector-set! memory index val)])))
+      [(not (in-range? mem index)) (report-memory-index-out-of-range! index (memory-size mem))]
+      [(member index (memory-free-addr-pool mem)) (report-unauthorized-access-to-memory! index)]
+      [else (vector-set! (memory-store mem) index val)])))
 
 (define get-val
-  (lambda (index)
+  (lambda (mem index)
     (cond
-      [(not (in-range? index)) (report-memory-index-out-of-range! index)]
-      [(member index free-addr-pool) (report-unauthorized-access-to-memory! index)]
-      [else (vector-ref memory index)])))
+      [(not (in-range? mem index)) (report-memory-index-out-of-range! index (memory-size mem))]
+      [(member index (memory-free-addr-pool mem)) (report-unauthorized-access-to-memory! index)]
+      [else (vector-ref (memory-store mem) index)])))
 
 (define free
-  (lambda (index)
+  (lambda (mem index)
     (cond
-      [(not (in-range? index)) (report-memory-index-out-of-range! index)]
-      [(member index free-addr-pool) (report-unauthorized-access-to-memory! index)]
-      [else (set! free-addr-pool (cons index free-addr-pool))])))
+      [(not (in-range? mem index)) (report-memory-index-out-of-range! index (memory-size mem))]
+      [(member index (memory-free-addr-pool mem)) (report-unauthorized-access-to-memory! index)]
+      [else (set-memory-free-addr-pool! mem (cons index (memory-free-addr-pool mem)))])))
 
 (provide (all-defined-out))
