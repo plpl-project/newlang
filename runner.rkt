b38c04a3f5e993401ddeeb52a814ce97a52c2e1c
ma1eck <amir.hossein.maleck.mohammady@gmail.com>
2025-08-31 14:08:08 +0330

    cleaned for phase2

---
+++ runner.rkt
@@ -0,0 +1,15 @@
+#lang racket
+
+(require rackunit)
+(require "lexer.rkt")
+(require "parser.rkt")
+(require "datatypes.rkt")
+(require "boo-lang.rkt")
+
+
+
+(define path "phase2_questions/q5.txt")
+(define (run filename)
+    (value-of-program (full-parser (lex-this my-lexer (open-input-file filename)))))
+(run path)
+
